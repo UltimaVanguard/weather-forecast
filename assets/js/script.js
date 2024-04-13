885eef234517cf0e5f582fc44cb769b70afb99a3
UltimaVanguard <ultimavanguard@gmail.com>
2024-04-13 10:50:14 -0500

    added some headings and text to html and added html element variables to JavaScript

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// JavaScript variables for HTML elements
+const searchInput = $('#search');
+const history = $('#history');
+const todayWeather = $('#today');
+const wthForecast = $('#five-day');
+
+// Get history from local storage or initialize empty array
+// let historyArray = JSON.parse(localStorage.getItem('history')) || [];
