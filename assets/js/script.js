0b22dd28c313b333c70b0b0c30b9fb850494f27b
UltimaVanguard <ultimavanguard@gmail.com>
2024-04-13 12:08:48 -0500

    saving to local storage, pulling from local storage, and display history buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,51 @@
 // JavaScript variables for HTML elements
-const searchForm = $('.search-form');
-const searchInput = $('#search');
-const history = $('#history');
-const todayWeather = $('#today');
-const wthForecast = $('#five-day');
+const searchFormEl = $('.search-form');
+const searchInputEl = $('#search');
+const historyEl = $('#history');
+const todayWeatherEl = $('#today');
+const wthForecastEl = $('#five-day');
 
 // Get history from local storage or initialize empty array
-// let historyArray = JSON.parse(localStorage.getItem('history')) || [];
+let historyArray = JSON.parse(localStorage.getItem('history')) || [];
+
+function displayHistory(history) {
+    const histButton = $('<button>');
+    histButton.addClass('button hist-btn');
+    histButton.attr('data-history', history);
+    histButton.text(history);
+
+    historyEl.append(histButton);
+}
+
+function saveHistory(location) {
+    console.log(location);
+    for (i = 0; i < historyArray.length; i++) {
+        if (location === historyArray[i]) {
+            historyArray.splice(i, 1);
+            historyArray.unshift(location);
+            return;
+        }
+    }
+
+    if (historyArray.length === 10) {
+        historyArray.pop();
+        historyArray.unshift(location);
+    } else {
+        historyArray.unshift(location);
+    }
+
+    localStorage.setItem('history', JSON.stringify(historyArray));
+}
+
+function updateHistory() {
+    $('.hist-btn').remove();
+
+    if (historyArray) {
+        for (let history of historyArray) {
+            displayHistory(history);
+        }
+    }
+}
 
 function getForecast(weather) {
     console.log('here is the weather')
@@ -15,7 +54,7 @@ function getForecast(weather) {
 function fetchWeather(event) {
     event.preventDefault();
 
-    const searchLocation = searchInput.val().substr(0,1).toUpperCase() + searchInput.val().substr(1);;
+    const searchLocation = searchInputEl.val().substr(0,1).toUpperCase() + searchInputEl.val().substr(1);;
     const weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${searchLocation}&APPID=4f194bed6606dee76ffaf3829b41e1e9`;
     console.log(weatherURL);
 
@@ -30,9 +69,11 @@ function fetchWeather(event) {
         .then(function(weather) {
             console.log(weather);
             getForecast(weather);
-            // saveHistory(searchLocation);
-            // updateHistory();
+            saveHistory(searchLocation);
+            updateHistory();
         })
 }
 
-searchForm.on('submit', fetchWeather);
+searchFormEl.on('submit', fetchWeather);
+
+window.load = (updateHistory());
